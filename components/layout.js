83c3c50fdc1097d36a97d25009fdb38ca4cc16a3
RobinAxelsson <axelsson.robin@outlook.com>
2022-12-28 12:45:22 +0100

    edit image size

--- components/layout.js
+++ components/layout.js
@@ -45,8 +45,8 @@ export default function Layout({ children, home }) {
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
+                  height={120}
+                  width={120}
                   alt=""
                 />
               </Link>
