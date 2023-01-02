1e0dfc4fb7492f2e704b32adf3d86025b86d5f3d
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-02 14:34:34 +0100

    Responsive layout good in mobile

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ export default function Home({ summaryData }) {
         <div className="hero-section-content">
           <h1><span className="accent-word">Robin</span> Salih Axelsson</h1>
           <h2>Full Stack Cloud Developer</h2>
-          <h2>Software by Quokka</h2>
           <p>Specialized in Object Oriented Design,</p>
           <p>.NET, C#, Test Driven Development, CAD</p>
         </div>
