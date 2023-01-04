0b920fd8a752e44659e6c799f76ec33c9e2f86ea
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-04 12:15:00 +0100

    refactored experience to components

--- pages/experience.js
+++ pages/experience.js
@@ -1,7 +1,8 @@
 import { getWorkLife, getEducation } from "../data-access/resume-repository";
 import { generateUID } from "../framework-utils/generate-uid";
 import siteDateLayout from "../framework-utils/site-date-layout";
-import CardContainer from "../components/micro-components/card-container"
+import CardContainer from "../components/micro-components/card-container";
+import Card from "../components/micro-components/card";
 
 export async function getStaticProps() {
   return {
@@ -15,48 +16,60 @@ export async function getStaticProps() {
 export default function ExperiencePage({ worklife, education }) {
   return (
     <CardContainer>
-      <div className="card-general">
-        <h2 className="card-title">Work Experience</h2>
-        <div className="card-grid grid-column-3">
-          {worklife.map(({ company, title, start, end }) => (
-            <>
-              <div className="card-grid-item" key={generateUID()}>
-                {siteDateLayout(start)} - {siteDateLayout(end)}
-              </div>
-              <div className="card-grid-item" key={generateUID()}>
-              {(() => {
-                  if (company !== "Software By Quokka") return company;
-                  return (<a href="https://quokka.se">Software By Quokka</a>)
-                })()}
-              </div>
-              <div className="card-grid-item" key={generateUID()}>
-                {title}
-              </div>
-            </>
-          ))}
-        </div>
-      </div>
-      <div className="card-general">
-        <h2 className="card-title">Education</h2>
-        <div className="card-grid grid-column-3">
-          {education.map(({ title, school, start, end }) => (
-            <>
-              <div className="card-grid-item" key={generateUID()}>
-                {siteDateLayout(start)} - {siteDateLayout(end)}
-              </div>
-              <div className="card-grid-item" key={generateUID()}>
-                {(() => {
-                  if (title !== ".NET Developer") return title;
-                  return (<a href="https://teknikhogskolan.se/utbildningar/net-utvecklare/">.NET Developer</a>)
-                })()}
-              </div>
-              <div className="card-grid-item" key={generateUID()}>
-                {school}
-              </div>
-            </>
-          ))}
-        </div>
-      </div>
+      <Card
+        right={
+          <>
+            <h2 className="card-title">Work Experience</h2>
+            <div className="card-grid grid-column-3">
+              {worklife.map(({ company, title, start, end }) => (
+                <>
+                  <div className="card-grid-item" key={generateUID()}>
+                    {siteDateLayout(start)} - {siteDateLayout(end)}
+                  </div>
+                  <div className="card-grid-item" key={generateUID()}>
+                    {(() => {
+                      if (company !== "Software By Quokka") return company;
+                      return <a href="https://quokka.se">Software By Quokka</a>;
+                    })()}
+                  </div>
+                  <div className="card-grid-item" key={generateUID()}>
+                    {title}
+                  </div>
+                </>
+              ))}
+            </div>
+          </>
+        }
+      />
+      <Card
+        right={
+          <>
+            <h2 className="card-title">Education</h2>
+            <div className="card-grid grid-column-3">
+              {education.map(({ title, school, start, end }) => (
+                <>
+                  <div className="card-grid-item" key={generateUID()}>
+                    {siteDateLayout(start)} - {siteDateLayout(end)}
+                  </div>
+                  <div className="card-grid-item" key={generateUID()}>
+                    {(() => {
+                      if (title !== ".NET Developer") return title;
+                      return (
+                        <a href="https://teknikhogskolan.se/utbildningar/net-utvecklare/">
+                          .NET Developer
+                        </a>
+                      );
+                    })()}
+                  </div>
+                  <div className="card-grid-item" key={generateUID()}>
+                    {school}
+                  </div>
+                </>
+              ))}
+            </div>
+          </>
+        }
+      />
     </CardContainer>
   );
 }
