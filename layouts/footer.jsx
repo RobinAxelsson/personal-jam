75d17ed74885ae929e4df268c4ebbb5969ba7055
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-10 15:44:54 +0100

    footer bem a bit more

--- layouts/footer.jsx
+++ layouts/footer.jsx
@@ -1,12 +1,22 @@
 export const Footer = () => {
   return (
-    <footer>
-      <h3>© 2023 Robin Salih Axelsson</h3>
-      <ul>
-        <a href="https://www.linkedin.com/in/axelsson-robin/">LinkedIn</a>
-        <a href="https://www.github.com/robinaxelsson">GitHub</a>
-        <a href="https://www.quokka.se/">Software by Quokka</a>
-        <span className="accent-word">robin.axelsson@quokka.se</span>
+    <footer className="site-footer">
+      <ul className="site-footer__list">
+        <li className="site-footer__item site-footer__item--title">
+          <h3>© 2023 Robin Salih Axelsson</h3>
+        </li>
+        <li className="site-footer__item site-footer__item--link">
+          <a href="https://www.linkedin.com/in/axelsson-robin/">LinkedIn</a>
+        </li>
+        <li className="site-footer__item site-footer__item--link">
+          <a href="https://www.github.com/robinaxelsson">GitHub</a>
+        </li>
+        <li className="site-footer__item site-footer__item--link">
+          <a href="https://www.quokka.se/">Software by Quokka</a>
+        </li>
+        <li className="site-footer__item site-footer__item--email">
+          <span className="accent-word">robin.axelsson@quokka.se</span>
+        </li>
       </ul>
     </footer>
   );
