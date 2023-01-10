7286b6c5b94c8d5e35e0ab38eb5492e19d816a1d
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-11 00:08:15 +0100

    removed parser seconds

--- data-parsers/parser-seconds.js
+++
@@ -1,6 +0,0 @@
-export default function parseSecondsToText(seconds) {
-  let h = Math.floor(seconds / 3600);
-  let m = Math.floor((seconds % 3600) / 60);
-  let s = Math.floor((seconds % 3600) % 60);
-  return `${h}h ${m}m ${s}s`;
-}
