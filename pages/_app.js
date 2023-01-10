54ffb06eba467c6e99d20fb9cb2f95a71f8ef69e
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-10 15:15:15 +0100

    wxtracteed wide-nav css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import MainLayout from "../layouts/main-layout";
 import "../styles/globals.css";
 import "../components/mobile-nav.css";
+import "../components/wide-nav.css";
 import "../styles/general.sass";
 
 function MyApp({ Component, pageProps }) {
