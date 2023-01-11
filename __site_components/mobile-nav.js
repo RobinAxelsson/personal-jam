b811cdfb09a1086c897a120d3395d8d2935b7e13
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-11 13:49:48 +0100

    encapsulated mobile-nav.module.css

--- __site_components/mobile-nav.js
+++ __site_components/mobile-nav.js
@@ -1,48 +1,49 @@
 import Link from "next/link";
+import styles from "./mobile-nav.module.css"
 
 export const MobileNav = () => (
-  <nav class="mobile-nav">
-    <input id="mobile-nav__toggle" type="checkbox" />
-    <label class="mobile-nav__btn" for="mobile-nav__toggle">
+  <nav className={styles.mobile_nav}>
+    <input id={styles.mobile_nav__toggle} type="checkbox" />
+    <label className={styles.mobile_nav__btn} for={styles.mobile_nav__toggle}>
       <span></span>
     </label>
-    <ul class="mobile-nav__box">
-      <li className="mobile-nav__link-item">
+    <ul className={styles.mobile_nav__box}>
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/" passHref>
           Start
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/projects" passHref>
           Projects
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/stats" passHref>
           Coding Stats
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/experience" passHref>
           Work Experience & Education
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/tech" passHref>
           Technologies
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/school-of-thought" passHref>
           School of Thought
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <Link href="/about" passHref>
           About
         </Link>
       </li>
-      <li className="mobile-nav__link-item">
+      <li className={styles.mobile_nav__link_item}>
         <a href="https://drive.google.com/file/d/1T6D66JRzcRjqYrN8KlE58GevxIs82P4g/view?usp=share_link">
           CV
         </a>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import MainLayout from "../__site_layouts/main-layout";
 import "../__site_styles/globals.css";
 import "../__site_styles/general.sass";
-import "../__site_components/mobile-nav.css";
+// import "../__site_components/mobile-nav.css";
 import "../__site_components/wide-nav.css";
 // import "../__site_components/tech-table.css";
 import "../__site_components/coding-stats.css";
