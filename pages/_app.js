a711ba79a23957dd67b5f806bcc75f7334152d0e
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-11 14:20:09 +0100

    added global scss and cleanedup redundant files

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import MainLayout from "../__site_layouts/main-layout";
-import "../__site_styles/globals.css";
-import "../__site_styles/general.sass";
+import "../__site_styles/global.scss";
 import "../__site_components/wide-nav.css";
 import "../__site_layouts/site-footer.css";
 import "../__site_layouts/site-header.css";
