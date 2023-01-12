c9e7670a67b3398cd6618bb6c3ff56bf155144e4
RobinAxelsson <axelsson.robin@outlook.com>
2023-01-12 12:57:05 +0100

    redirect cv page

---
+++ pages/cv.js
@@ -0,0 +1,11 @@
+import MarkdownCard from "../__site_components/markdown-card"
+
+export default function CvPage() {
+  return (
+    <div className="card-collection">
+      <MarkdownCard markdownContent={"### [Download pdf robin-sa-cv-2023Q1.pdf](/robin-sa-cv-2023Q1.pdf)"}/>
+        </div>
+  );
+}
+
+
