d02e0a3a96cf4cfac0ab67884edf90f26563592a
RobinAxelsson <axelsson.robin@outlook.com>
2024-02-24 14:46:54 +0100

    feat: new 2024 CV

--- pages/cv.js
+++ pages/cv.js
@@ -3,7 +3,7 @@ import MarkdownCard from "../__site_components/markdown-card"
 export default function CvPage() {
   return (
     <div className="card-collection">
-      <MarkdownCard markdownContent={"### [Download pdf robin-sa-cv-2023Q1.pdf](/robin-sa-cv-2023Q1.pdf)"}/>
+      <MarkdownCard markdownContent={"### [Download pdf robin-sa-cv-2024Q1.pdf](/robin-sa-cv-2024Q1.pdf)"}/>
         </div>
   );
 }
