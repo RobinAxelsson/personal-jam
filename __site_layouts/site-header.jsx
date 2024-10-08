65acaa0e8f14ab48437607cf05b75a3824b72be6
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-10-08 09:34:05 +0200

    feat: hiding nav bar

--- __site_layouts/site-header.jsx
+++ __site_layouts/site-header.jsx
@@ -1,10 +1,35 @@
 import { MobileNav } from "../__site_components/mobile-nav";
 import { WideNav } from "../__site_components/wide-nav";
 import styles from './site-header.module.scss'
+import { useState, useEffect } from 'react';
 
 export const Header = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY && window.scrollY > 20) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <header className={styles.site_header}>
+    <header className={`${styles.site_header} ${showNavbar ? '' : styles.site_header_hidden}`}>
       <WideNav navLinks={navLinks} />
       <MobileNav navLinks={navLinks}/>
     </header>
